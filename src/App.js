801eca2be5f98e1c7aede5c0cda7461b1b4bdadc
ArmeDiazC <armediazc@gmail.com>
2022-06-11 16:22:39 -0500

    consume pokemon API

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [pokemon, setPokemon] = useState("");
+  const [pokemons, setPokemons] = useState();
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon")
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data);
+        setPokemons(data);
+      }).catch((error)=>console.error(error))
+  }, []);
+
+  const findPokemon = () => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+      .then((resp) => resp.json())
+      .then((data) => console.log(data))
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://pokeapi.co/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Gotta Catch'Em All!!!!
-        </a>
-      </header>
+      <input
+        type="text"
+        onChange={({ target }) => setPokemon(target.value)}
+        value={pokemon}
+      ></input>
+      <button onClick={findPokemon}>Buscar</button>
+      <div></div>
+      <div>
+        {pokemons?.results.map((poke) => (
+          <div><a href={poke.url} key={poke.name}>{poke.name}</a></div>
+        ))}
+      </div>
     </div>
   );
 }
