16f916ce77ef78f9ba845201023fd9894742b2f5
ArmeDiazC <armediazc@gmail.com>
2022-06-13 22:34:57 -0500

    add test

--- src/pages/__tests__/Home.test.js
+++ src/pages/__tests__/Home.test.js
@@ -1,6 +1,13 @@
-import { render, screen } from "@testing-library/react";
+import {
+  act,
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
 import Home from "../Home";
 import { BrowserRouter } from "react-router-dom";
+
+jest.setTimeout(50000)
 
 test("renders Home button", () => {
   render(
@@ -18,8 +25,22 @@ test("renders loading", async () => {
       <Home />
     </BrowserRouter>
   );
-  const card = await screen.getByTestId('spinner')
-  expect(card).toBeInTheDocument();
-  
+  const spinner = await screen.getByTestId("spinner");
+  expect(spinner).toBeInTheDocument();
 });
 
+test("renders Cards", async () => {
+  render(
+    <BrowserRouter>
+      <Home />
+    </BrowserRouter>
+  );
+  await waitForElementToBeRemoved(screen.getByTestId("spinner"), { timeout: 45000 });
+
+  //await waitForElementToBeRemoved(async() => await screen.getByTestId("spinner"));
+
+  const selectElement = screen.getByDisplayValue('grass')
+
+  //const card = screen.getByTestId("card");
+  expect(selectElement).toBeInTheDocument();
+});
